6d0abe8c19db25cc0d331cbf1561d0da7900943f
tomeh2 <tomislav.harmina@gmail.com>
2022-08-12 12:35:17 +0200

    First commit for system test program

--- PICORV32/RTL/PICORV32/picorv32.v
+++ PICORV32/RTL/PICORV32/picorv32.v
@@ -85,7 +85,7 @@ module picorv32 #(
 	parameter [31:0] LATCHED_IRQ = 32'h ffff_ffff,
 	parameter [31:0] PROGADDR_RESET = 32'h 0000_0000,
 	parameter [31:0] PROGADDR_IRQ = 32'h 0000_0000,
-	parameter [31:0] STACKADDR = 32'h 1000_1000
+	parameter [31:0] STACKADDR = 32'h 2000_1000
 ) (
 	input clk, resetn,
 	output reg trap,
