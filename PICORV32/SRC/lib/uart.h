13fbbd48f297316da3138898e6fa6f590dc8855d
tomeh2 <tomislav.harmina@gmail.com>
2022-09-02 11:29:05 +0200

    Removed SRC dirs from NEORV and PICORV

--- PICORV32/SRC/lib/uart.h
+++
@@ -1,4 +0,0 @@
-void uart_init();
-void uart_putc(char chr);
-void uart_puts(char* str);
-void format_hex(unsigned int num, char* dest);
--- PICORV32/SRC/projects/system_test/start.s
+++
@@ -1,6 +0,0 @@
-.global _start
-
-.extern bootloader_entry
-
-_start:
-	jal bootloader_entry
